51d66350edd90c7e9dce25445fb7a40e47894949
charleslofblad <charleslofblad@gmail.com>
2016-03-23 15:45:20 +0100

    stadsbusdsite verson 7

--- stadsbudsite/urls.py
+++ stadsbudsite/urls.py
@@ -20,5 +20,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.article_list, name='article_list'),
+    #url(r'^$', views.article_list, name='article_list'),
+    url(r'^news/', views.article_list, name='article_list'),
+    url(r'^news(?P<pk>\d+)/$', views.article_detail, name='article_detail'),
+    url(r'^$', views.index_view, name='index_view'),
+
 ]
--- stadsbudsite/views.py
+++ stadsbudsite/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Article
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 #def article_list(request):
@@ -9,3 +10,10 @@ from .models import Article
 def article_list(request):
     articles = Article.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'article/article_list.html', {'articles': articles})
+
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, 'article/article_detail.html', {'article': article})
+
+def index_view(request):
+    return render(request, 'index.html', {})
